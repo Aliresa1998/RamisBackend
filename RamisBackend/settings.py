08731f6f95a563f8d21889e2495ab5910b817c92
Sina-Lotfi <sinalotfi531@gmail.com>
2023-04-11 21:02:23 +0330

    make profile endpoint

--- RamisBackend/settings.py
+++ RamisBackend/settings.py
@@ -83,10 +83,15 @@ WSGI_APPLICATION = 'RamisBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ramis',
+        'USER': 'admin',
+        'PASSWORD': 'admin12345689',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
