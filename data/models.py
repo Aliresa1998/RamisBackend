52115b82ef1d851d330d528fb5873731431716a1
mahdizo3181 <mahdizolfaghari7908@gmail.com>
2023-04-14 11:24:42 +0330

    Feat  | model crypto

--- data/models.py
+++ data/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class Crypto(models.Model):
+    name = models.CharField(max_length=15)
+    image_url = models.ImageField(null=True, blank=True)
