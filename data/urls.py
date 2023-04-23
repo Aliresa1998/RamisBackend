1507e6330121d3c4f2e78b1464bc9db84ccc36be
mahdizo3181 <mahdizolfaghari7908@gmail.com>
2023-04-23 21:13:17 +0330

    Feat | trade router

--- data/urls.py
+++ data/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import OhlcData, CryptoList
+from .views import OhlcData, CryptoList, CreateTrade
 
 urlpatterns = [
     path('data/', OhlcData.as_view(), name='data'),
-    path('crypto_list/', CryptoList.as_view(), name='crypto_list')
+    path('crypto_list/', CryptoList.as_view(), name='crypto_list'),
+    path('trade/', CreateTrade.as_view(), name='create_trade'),
 ]
