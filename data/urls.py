3767c48d1314940542493fbb50cdd3d622d37139
mahdizo3181 <mahdizolfaghari7908@gmail.com>
2023-04-28 18:12:37 +0330

    Feat | history and wallet and Withdraw Wallet router

--- data/urls.py
+++ data/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import OhlcData, CryptoList, CreateTrade, Historytrade, UpdateHistoryTrade
+from .views import OhlcData, CryptoList, CreateTrade, Historytrade, UpdateHistoryTrade, CreateWallet, WithdrawWallet
 
 urlpatterns = [
     path('data/', OhlcData.as_view(), name='data'),
     path('crypto_list/', CryptoList.as_view(), name='crypto_list'),
     path('trade/', CreateTrade.as_view(), name='create_trade'),
     path("history/", Historytrade.as_view(), name='history'),
-    path("history/<int:pk>", UpdateHistoryTrade.as_view(), name='history')
+    path("history/<int:pk>", UpdateHistoryTrade.as_view(), name='history'),
+    path("wallet/", CreateWallet.as_view(), name='wallet'),
+    path("withdraw/", WithdrawWallet.as_view(), name='withdraw_wallet'),
 ]
