c1fca68e7beef04f1803d724309be9e8a5a06286
Sina-Lotfi <sinalotfi531@gmail.com>
2023-05-11 20:44:19 +0330

    Feat | add get-challange url to get chllange

--- data/urls.py
+++ data/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import AccountGrowthView, ChallangeView, OhlcData, CryptoList, CreateTrade, Historytrade, UpdateHistoryTrade, CreateWallet, WalletHistoryView, WithdrawWallet, \
+from .views import AccountGrowthView, GetChallangeView, UpgradeChallangeView, OhlcData, CryptoList, CreateTrade, Historytrade, UpdateHistoryTrade, CreateWallet, WalletHistoryView, WithdrawWallet, \
     GetWallet
 
 urlpatterns = [
@@ -8,10 +8,11 @@ urlpatterns = [
     path('trade/', CreateTrade.as_view(), name='create_trade'),
     path("history/", Historytrade.as_view(), name='history'),
     path("history/<int:pk>", UpdateHistoryTrade.as_view(), name='history'),
-    path("wallet/", CreateWallet.as_view(), name='wallet'),
+    path("deposit-wallet/", CreateWallet.as_view(), name='wallet'),
     path("withdraw/", WithdrawWallet.as_view(), name='withdraw_wallet'),
     path('get-wallet/', GetWallet.as_view(), name='get_wallet'),
     path('wallet-history/', WalletHistoryView.as_view(), name='wallet-history'),
-    path('challange-upgrade/', ChallangeView.as_view(), name='upgrade-challange'),
+    path('challange-upgrade/', UpgradeChallangeView.as_view(), name='upgrade-challange'),
+    path('get-challange/', GetChallangeView.as_view(), name='get-challange'),
     path('account-growth/', AccountGrowthView.as_view(), name='account-growth'),
 ]
