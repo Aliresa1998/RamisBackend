e6349cb740ae722ecb674cf8bb70866cbefc9c53
mahdizo3181 <mahdizolfaghari7908@gmail.com>
2023-05-13 21:54:58 +0330

    Refactor | data tasks

---
+++ data/tasks.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from celery import shared_task
+from .models import Trade, Wallet
+import yfinance as yf
+import decimal
+
+
+@shared_task
+def five_minute_long():
+    trades = Trade.objects.filter(status=True)
+    for trade in trades:
+        exit_price = yf.Ticker(trade.symbol).history()['Close'][-1]
+        exit_price = decimal.Decimal(exit_price)
+        if exit_price >= trade.take_profit or exit_price <= trade.exit_price:
+            pnl = (exit_price - trade.entry_price) * trade.amount
+            Trade.objects.filter(id=trade.id).update(exit_price=exit_price, pnl=pnl, close_time=datetime.now(),
+                                                     status=False, )
+            wallet_balance = Wallet.objects.get(user=trade.user).balance
+            new_wallet_balance = wallet_balance + pnl
+            Wallet.objects.filter(user=trade.user).update(balance=new_wallet_balance)
