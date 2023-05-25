e1ebc260026af54448d2ae3667a56fba12f34b6d
mahdizo3181 <mahdizolfaghari7908@gmail.com>
2023-05-25 20:21:49 +0330

    Refactor | setting

--- RamisBackend/settings.py
+++ RamisBackend/settings.py
@@ -96,20 +96,14 @@ WSGI_APPLICATION = 'RamisBackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'ramis',
-#         'USER': 'admin',
-#         'PASSWORD': 'admin12345689',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ramis',
+        'USER': 'admin',
+        'PASSWORD': 'admin12345689',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 # Password validation
