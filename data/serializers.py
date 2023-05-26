99fab5191b47a5c12c960d52c129a2e9033432c8
mahdizo3181 <mahdizolfaghari7908@gmail.com>
2023-05-26 21:50:10 +0330

    Refactor | Trade serializers

--- data/serializers.py
+++ data/serializers.py
@@ -31,17 +31,20 @@ class TradeSerializer(serializers.ModelSerializer):
         take_profit = validated_data.get('take_profit')
         direction = validated_data.get('direction')
         entry_price = validated_data.get('entry_price')
-        if stop_loss and stop_loss >= entry_price:
+        if direction == 'LONG' and stop_loss and stop_loss >= entry_price:
             raise serializers.ValidationError({'stop_loss': 'Stop loss price must be below entry price.'})
-        if take_profit and take_profit <= entry_price:
+        if direction == 'LONG' and take_profit and take_profit <= entry_price:
             raise serializers.ValidationError({'take_profit': 'Take profit price must be above entry price.'})
+        if direction == 'SHORT' and stop_loss and stop_loss <= entry_price:
+            raise serializers.ValidationError({'stop_loss': 'Stop loss price must be above entry price.'})
+        if direction == 'SHORT' and take_profit and take_profit >= entry_price:
+            raise serializers.ValidationError({'take_profit': 'Take profit price must be below entry price.'})
         if direction == 'LONG' and stop_loss and take_profit and stop_loss >= take_profit:
             raise serializers.ValidationError(
                 {'non_field_errors': 'For long trades, stop loss must be below take profit.'})
         if direction == 'SHORT' and stop_loss and take_profit and stop_loss <= take_profit:
             raise serializers.ValidationError(
                 {'non_field_errors': 'For short trades, stop loss must be above take profit.'})
-
         return super().create(validated_data)
 
 
