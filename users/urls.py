4fabe2275e8e2c2154ab15d7b0ac82e908f91251
Sina-Lotfi <sinalotfi531@gmail.com>
2023-06-04 13:22:27 +0330

    Feat | add editinformation url

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,7 @@ from django.urls import path, re_path
 from dj_rest_auth.registration.views import RegisterView, VerifyEmailView, ConfirmEmailView
 from dj_rest_auth.views import LoginView, LogoutView
 from rest_framework import routers
-from .views import AdminChangePassowrdView, AllProfileView, ProfileViewSet, SendMessageAPIView, InboxAPIView, EditUserNameView, \
-    CustomPasswordChangeView, AdminEditUserNameView
+from .views import AdminChangePassowrdView, AllProfileView, EditInformationView, ProfileViewSet, SendMessageAPIView, InboxAPIView, AdminEditUserNameView
 
 router = routers.DefaultRouter()
 router.register('', ProfileViewSet, basename='profile')
@@ -15,10 +14,9 @@ urlpatterns = [
                   path('all_profile/', AllProfileView.as_view()),
                   path('send-message/', SendMessageAPIView.as_view(), name='send_message'),
                   path('inbox/', InboxAPIView.as_view(), name='inbox'),
-                  path('edit_user_name/', EditUserNameView.as_view(), name='edit_user_name'),
-                  path('password_change/', CustomPasswordChangeView.as_view(), name='password'),
                   path('admin/change-username/', AdminEditUserNameView.as_view(), name='admin-edit-username'),
                   path('admin/change-password/', AdminChangePassowrdView.as_view(), name='admin-change-password'),
+                  path('edit-information', EditInformationView.as_view(), name='edit-informations'),
                   path('verify-email/',
                        VerifyEmailView.as_view(), name='account_email'),
                   path('account-confirm-email/',
