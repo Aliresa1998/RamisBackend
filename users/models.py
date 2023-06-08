34014a7bf7aa3bd4cc2849ad693f0c5e2ab0a323
Sina-Lotfi <sinalotfi531@gmail.com>
2023-06-08 20:50:35 +0330

    Feat | add document model

--- users/models.py
+++ users/models.py
@@ -1,9 +1,12 @@
+import email
+from enum import Flag
 from django.db import models
 
 from django.db import models
 from django.core.validators import MinLengthValidator, RegexValidator
 from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
+from numpy import identity
 
 from users.validators import validate_image_size
 
@@ -12,15 +15,15 @@ class CustomUser(models.Model):
     first_name = models.CharField(max_length=200, null=True, blank=True)
     last_name = models.CharField(max_length=200, null=True, blank=True)
     birth_date = models.DateField(null=True, blank=True)
-    image = models.ImageField(
-        validators=[validate_image_size], null=True, blank=True)
-    country = models.CharField(max_length=50, validators=[
+    address = models.CharField(max_length=50, validators=[
         MinLengthValidator(3)], null=True, blank=True)
     phone_regex = RegexValidator(
         regex=r'^\+?1?\d{9,15}$',
         message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number = models.CharField(
         validators=[phone_regex], max_length=17, blank=True, null=True)
+    national_code = models.CharField(null=True, blank=True, max_length=15)
+    email = models.CharField(null=True, blank=True, max_length=250)
     is_admin = models.BooleanField(default=False)
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, related_name='profile')
@@ -53,8 +56,20 @@ class Ticket(models.Model):
         User, on_delete=models.CASCADE, related_name='sender_ticket')
     receiver = models.CharField(null=True, blank=True, max_length=30)
     body = ArrayField(models.CharField(), blank=True, null=True)
-    status = models.CharField(choices=STATUS_CHOISES, default='Open')
+    status = models.CharField(choices=STATUS_CHOISES, default='open')
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-created_at']
+
+class Document(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='document')
+    profile_image = models.ImageField(
+        validators=[validate_image_size], null=True, blank=True)
+    identity_card = models.ImageField(
+        validators=[validate_image_size], null=True, blank=True)
+    birth_certificate = models.ImageField(
+        validators=[validate_image_size], null=True, blank=True)
+    Commitment_letter = models.ImageField(
+        validators=[validate_image_size], null=True, blank=True)
