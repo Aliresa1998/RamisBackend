d83b818ec89958e1cddd64a87573a5f3a7c00001
mahdizo3181 <mahdizolfaghari7908@gmail.com>
2023-07-28 20:12:31 +0330

    Refactor | plan serializer

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,8 @@ from .views import AdminChangePassowrdView, AdminCloseTicketView, AdminCreateTic
     AllProfileView, DocumentView, EditInformationView, ExportProfilesPDFView, MessageIsReadView, ProfileViewSet, \
     SendMessageAPIView, InboxAPIView, \
     AdminEditUserNameView, TicketIsReadView, UserCloseTicketView, UserCreateTicketView, UserTicketMessageView, \
-    GetTicketBYID, ProfilePictureUpdate, GetInboxByID, IsAdminView, Unread, PlanView, GetPlan, GetDocumentById
+    GetTicketBYID, ProfilePictureUpdate, GetInboxByID, IsAdminView, Unread, PlanView, GetPlan, GetDocumentById, \
+    PlanVerifyView
 
 router = routers.DefaultRouter()
 router.register('', ProfileViewSet, basename='profile')
@@ -37,8 +38,9 @@ urlpatterns = [
                   path('admin/create-ticket/', AdminCreateTicketView.as_view(), name='admin-create-ticket'),
                   path('admin/status-ticket/', AdminCloseTicketView.as_view(), name='admin-close/open-ticket'),
                   path('unread/<str:type>', Unread.as_view(), name='unread'),
-                  path('plan', PlanView.as_view(), name='plan'),
+                  path('plan', PlanView.as_view(), name='go_to_gateway_view'),
                   path('getplan', GetPlan.as_view(), name='get_plan'),
+                  path("planverifyview/", PlanVerifyView.as_view(), name='verify_view'),
                   path("getdocument/<str:user>", GetDocumentById.as_view(), name='get_document'),
                   path('is-admin/', IsAdminView.as_view(), name='is-admin'),
                   path('verify-email/',
