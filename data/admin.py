0475ec9a0eca99e6a7b55bdeae5166140b71568e
mahdizo3181 <mahdizolfaghari7908@gmail.com>
2023-08-10 19:43:28 +0330

    Refactor | data admin

--- data/admin.py
+++ data/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import AccountGrowth
+from .models import AccountGrowth, Trade
+
+
 # Register your models here.]@admin.register(CustomUser)
 
 @admin.register(AccountGrowth)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'date', 'balance']
+
+
+admin.site.register(Trade)
