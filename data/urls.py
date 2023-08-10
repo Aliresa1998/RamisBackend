9589a8b89bc9691be72824dd599f25d2dba88b88
mahdizo3181 <mahdizolfaghari7908@gmail.com>
2023-08-10 19:42:34 +0330

    Refactor | data url

--- data/urls.py
+++ data/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import AccountGrowthView, GetChallangeView, UpgradeChallangeView, OhlcData, CryptoList, CreateTrade, Historytrade, UpdateHistoryTrade, CreateWallet, WalletHistoryView, WithdrawWallet, \
+from .views import AccountGrowthView, GetChallangeView, UpgradeChallangeView, OhlcData, CryptoList, CreateTrade, \
+    Historytrade, UpdateHistoryTrade, CreateWallet, WalletHistoryView, WithdrawWallet, \
     GetWallet
 
 urlpatterns = [
@@ -7,7 +8,7 @@ urlpatterns = [
     path('crypto_list/', CryptoList.as_view(), name='crypto_list'),
     path('trade/', CreateTrade.as_view(), name='create_trade'),
     path("history/<str:type>", Historytrade.as_view(), name='history'),
-    path("close/<int:pk>", UpdateHistoryTrade.as_view(), name='history'),
+    path("close/<int:pk>/", UpdateHistoryTrade.as_view(), name='history_without_leverage'),
     path("deposit-wallet/", CreateWallet.as_view(), name='wallet'),
     path("withdraw/", WithdrawWallet.as_view(), name='withdraw_wallet'),
     path('get-wallet/', GetWallet.as_view(), name='get_wallet'),
