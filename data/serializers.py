162f89af4805afd3ddca52afa04ce6ac9803a849
mahdizo3181 <mahdizolfaghari7908@gmail.com>
2023-08-27 21:07:58 +0330

    Refactor

--- data/serializers.py
+++ data/serializers.py
@@ -128,4 +128,4 @@ class OrderSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Order
-        fields = ['user', 'order_type', 'price', 'amount', 'symbol']
+        fields = ['user', 'order_type', 'price', 'amount', 'symbol', "is_delete"]
