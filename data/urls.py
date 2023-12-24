9962136f2b6b5a379912017c63285aa2a65ac916
mahdizo3181 <mahdizolfaghari7908@gmail.com>
2023-12-24 22:25:46 +0330

    Refactor | tasks data

--- data/urls.py
+++ data/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from .views import AccountGrowthView, GetChallangeView, UpgradeChallangeView, OhlcData, CryptoList, CreateTrade, \
     Historytrade, UpdateHistoryTrade, CreateWallet, WalletHistoryView, WithdrawWallet, \
-    GetWallet, PlaceOrderView, GetOrder, DeleteOrder
+    GetWallet, PlaceOrderView, GetOrder, DeleteOrder ,CloseHistoryTrade
 
 urlpatterns = [
     path('data/', OhlcData.as_view(), name='data'),
     path('crypto_list/', CryptoList.as_view(), name='crypto_list'),
     path('trade/', CreateTrade.as_view(), name='create_trade'),
     path("history/<str:type>", Historytrade.as_view(), name='history'),
-    path("close/<int:pk>/", UpdateHistoryTrade.as_view(), name='history_without_leverage'),
+    path("close/<int:pk>/", CloseHistoryTrade.as_view(), name='history_without_leverage'),
     path("deposit-wallet/", CreateWallet.as_view(), name='wallet'),
     path("withdraw/", WithdrawWallet.as_view(), name='withdraw_wallet'),
     path('get-wallet/', GetWallet.as_view(), name='get_wallet'),
@@ -19,5 +19,7 @@ urlpatterns = [
     path('account-growth/', AccountGrowthView.as_view(), name='account-growth'),
     path('place-order/', PlaceOrderView.as_view(), name='place-order'),
     path('get-order/', GetOrder.as_view(), name='get-order'),
-    path('delete-order/<int:order_id>', DeleteOrder.as_view(), name='delete')
+    path('delete-order/<int:order_id>', DeleteOrder.as_view(), name='delete'),
+    path("update/<int:pk>/", UpdateHistoryTrade.as_view(), name=''),
+
 ]
