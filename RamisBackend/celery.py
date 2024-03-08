831c20b76f832d355b20edf553dde2cc2a0973bb
abbasi.bahamin <abbasi.bahamin@gmail.com>
2024-03-08 16:52:17 +0330

    celery test

--- RamisBackend/celery.py
+++ RamisBackend/celery.py
@@ -2,6 +2,7 @@
 import os
 from celery import Celery
 from celery.schedules import crontab
+from datetime import datetime, timedelta
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RamisBackend.settings')
 
@@ -11,11 +12,14 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Autodiscover tasks in applications' tasks.py
 app.autodiscover_tasks()
 
+run_time = datetime.now() + timedelta(minutes=2)
+
 # Schedule
 app.conf.beat_schedule = {
     'get_user_total_balance_every_midnight': {
         'task': 'your_app_name.tasks.get_user_total_balance',
-        'schedule': crontab(hour=0, minute=0),
+        'schedule': run_time,
+        # 'schedule': crontab(hour=0, minute=0),
         # Optionally, you can add arguments to 'args': (arg1, arg2...)
     },
 }
