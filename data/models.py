9b25805d5b88db51fe250326a1dd21edcc4652fc
abbasi.bahamin <abbasi.bahamin@gmail.com>
2024-03-24 21:05:26 +0330

    order amount changed from int to decimal

--- data/models.py
+++ data/models.py
@@ -131,7 +131,7 @@ class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     order_type = models.CharField(max_length=20, choices=ORDER_TYPES)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    amount = models.IntegerField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
     symbol = models.CharField(max_length=20)
     value = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
